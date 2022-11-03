daff930ca8040e5f6887c99fb7cea8384a7d6af1
Igor Poliakov <i-sonic@yandex.ru>
2022-11-03 11:02:42 +0300

    Updated DEBUG in settings

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', default='password')
 
-DEBUG = os.getenv('DEBUG', default='False') == 'True'
+DEBUG = os.getenv('DEBUG', default=False)
 
 ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', default='*')
 
